42b00ee552b7e71c00f411b20611a0b59f9ebfce
Kevin Murphy <kevin@hxrsmurf.info>
2022-05-04 20:26:58 -0400

    Add try/except to website.py

--- website.py
+++ website.py
@@ -4,16 +4,18 @@ from lookup import number_lookup
 app = Flask(__name__)
 Bootstrap(app)
 
-result = None
-
 @app.route('/')
 def index():
   return render_template('index.html')
 
 @app.route('/search', methods = ['POST'])
 def search():
-  result = number_lookup(request.form['number'])
-  return render_template('index.html', lookup_result = result)
+  try:
+    result = number_lookup(request.form['number'])
+    return render_template('index.html', lookup_result = result)
+  except Exception as e:
+    print(e)
+    return render_template('index.html', lookup_result = 'Invalid Number')
 
 if __name__ == '__main__':
     app.run(debug=True)
